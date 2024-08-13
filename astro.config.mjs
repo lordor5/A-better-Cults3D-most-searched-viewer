217c366ee7b0c229fed67b5391a905d0d6718eea
Lordor5 <thelordor5@gmail.com>
2024-08-13 15:00:54 +0200

    maybe not github will build the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://lordor5.github.io",
+  base: "A-better-Cults3D-most-searched-viewer",
 });
