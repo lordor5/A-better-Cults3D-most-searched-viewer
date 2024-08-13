37512a1fba8af1a74af7c90b6a25eef98f6787c1
Lordor5 <thelordor5@gmail.com>
2024-08-14 00:37:30 +0200

    does it compiles on github?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://lordor5.github.io",
   base: "A-better-Cults3D-most-searched-viewer",
+  integrations: [sitemap()],
 });
